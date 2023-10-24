71baacc7ae3ab0a4d8ba578450cea2d23c8950ad
Franck Pommereau <franck.pommereau@gmail.com>
2023-10-24 17:59:40 +0200

    migrated hyena.reach to typer/rich interface

--- hyena/reach.py
+++ hyena/reach.py
@@ -1,18 +1,21 @@
 import collections
 import importlib
-import sys
 import json
 
-from colorama import Fore as F, Style as S
-from .simul import Event, Trans, tree
+from .simul import Event, Trans
+
+from typing import Annotated, Optional
+from typer import Typer, Option, FileTextWrite, Exit
+from rich.console import Console
+from rich.text import Text
 
 
 class AssertFailed(Exception):
-    def __init__(self, prop, state, exc=None):
+    def __init__(self, prop, state, err=None):
         super().__init__(f"assert {prop} failed on state {state}")
         self.prop = prop
         self.state = state
-        self.exc = exc
+        self.err = err
 
 
 class Explorer:
@@ -50,15 +53,6 @@ class Explorer:
 
     def __len__(self):
         return len(self.succ)
-
-    def progress(self, newline=False):
-        sys.stdout.write(f"\r{F.YELLOW}...{F.RESET} {len(self)} state"
-                         f"{'s' if len(self) > 1 else ''}"
-                         f" {S.DIM}+ {len(self.todo)} to explore"
-                         f"{S.RESET_ALL}")
-        if newline:
-            sys.stdout.write("\n")
-        sys.stdout.flush()
 
     def compile(self, props):
         for num, prop in enumerate(props):
@@ -99,75 +93,127 @@ class Explorer:
             old_paths = new_paths
         return []
 
+    def status(self):
+        return (f"[green]{len(self)}[/] state{'s' if len(self) > 1 else ''}"
+                f" explored ([green]+{len(self.todo)}[/] to go)")
 
-if __name__ == "__main__":
-    import argparse
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-c", "--class", dest="cls", type=str, metavar="CLASS",
-                        default="hyena.ena.System",
-                        help=("System class to be loaded"
-                              " (default: `hyena.ena.System`)"))
-    parser.add_argument("-j", "--json", type=str, metavar="PATH",
-                        help="JSON file to be loaded")
-    parser.add_argument("-p", "--python", type=str, metavar="PATH",
-                        help="Python template to be loaded")
-    parser.add_argument("-v", "--verbose", default=False, action="store_true",
-                        help="print states as they are explored")
-    parser.add_argument("-a", "--assert", default=[], metavar="EXPR",
-                        action="append", dest="props",
-                        help="property to check on state")
-    parser.add_argument("-t", "--trace", default=False, action="store_true",
-                        help="print a trace to a state found violating assert")
-    parser.add_argument("-s", "--save", default=None, metavar="PATH",
-                        type=argparse.FileType("w"),
-                        help="save state-space as JSON to PATH")
-    parser.add_argument("-l", "--limit", default=None, type=int, metavar="NUM",
-                        help="limit exploration to NUM states")
-    parser.add_argument("-d", "--depth", default=False, action="store_true",
-                        help=("explore depth-first instead of"
-                              " the default breadth-first"))
-    args = parser.parse_args()
-    modname, classname = args.cls.rsplit(".", 1)
+
+def state_text(state):
+    txt = Text(str(state))
+    txt.highlight_regex(r"\d+", "green")
+    txt.highlight_regex(r"\w+(?=\[)", "blue bold")
+    txt.highlight_regex(r"\w+(?=\=)", "blue")
+    return txt
+
+
+app = Typer(context_settings={"help_option_names": ["-h", "--help"]})
+
+
+@app.command()
+def main(
+    json: Annotated[
+        str,
+        Option(
+            "--json", "-j",
+            metavar="PATH",
+            help="JSON file to be loaded"
+        )],
+    python: Annotated[
+        str,
+        Option(
+            "--python", "-p",
+            metavar="PATH",
+            help="Python template to be loaded"
+        )],
+    cname: Annotated[
+        str,
+        Option(
+            "--class", "-c",
+            metavar="CLASS",
+            help="`System` class to be loaded"
+        )] = "hyena.ena.System",
+    verbose: Annotated[
+        bool,
+        Option(
+            "--verbose", "-v",
+            help="prints states as they are explored"
+        )] = False,
+    props: Annotated[
+        list[str],
+        Option(
+            "--assert", "-a",
+            help="properties to be checked on every state"
+        )] = [],
+    trace: Annotated[
+        bool,
+        Option(
+            "--trace", "-t",
+            help="print a trace to a state found violating assert"
+        )] = False,
+    save: Annotated[
+        FileTextWrite,
+        Option(
+            "--save", "-s",
+            metavar="PATH",
+            help="save state-space as JSON to PATH"
+        )] = None,
+    limit: Annotated[
+        Optional[int],
+        Option(
+            "--limit", "-l",
+            metavar="NUM",
+            help="limit exploration to NUM states"
+        )] = None,
+    depth: Annotated[
+        bool,
+        Option(
+            "--depth", "-d",
+            help="explore depth-first instead of the default breadth-first"
+        )] = False):
+    stdout, stderr = Console(), Console(stderr=True)
+    modname, classname = cname.rsplit(".", 1)
     try:
         ena = importlib.import_module(modname)
         cls = getattr(ena, classname)
     except Exception as err:
-        parser.exit(2, (f"could not import '{modname}.{classname}'"
-                        f" ({err.__class__.__name__}: {err})\n"))
-    system = cls.from_json(args.json, args.python)
-    explorer = None
+        stderr.print(f"[red bold]error:[/] could not import",
+                     f"'{modname}.{classname}'")
+        stderr.print(f" :boom: [red]{err.__class__.__name__}[/]: {err}")
+        raise Exit(2)
+    system = cls.from_json(json, python)
     try:
-        explorer = Explorer(system,
-                            depth=args.depth,
-                            limit=args.limit,
-                            props=args.props)
-        for state, succs in explorer:
-            if args.verbose:
-                print(state, f"{S.DIM}=> +{len(succs)}{S.RESET_ALL}")
-            explorer.progress(args.verbose)
-    except KeyboardInterrupt:
-        pass
+        explorer = Explorer(system, depth, limit, props)
     except AssertFailed as err:
-        lines = []
-        if not args.verbose and explorer is not None and len(explorer):
-            print()
-        if err.state is None:
-            lines = [f"{F.RED}invalid assert:{F.RESET} {err.prop}"]
-        else:
-            lines = [f"{F.RED}assert failed:{F.RESET} {err.prop}"]
-            lines.append(f"{F.RED}on state:{F.RESET} {err.state}")
-        if err.exc is not None:
-            lines.append(f"{F.RED}raised {err.exc.__class__.__name__}:"
-                         f"{F.RESET} {err.exc}")
-        if args.trace and explorer is not None and err.state is not None:
-            print(f"{F.RED}### trace ###{F.RESET}")
-            for n, e in enumerate(explorer.trace(err.state)):
-                if e.trans is not None:
-                    print(f"{F.RED}>>>{F.RESET} system.{e.trans}"
-                          f" {S.DIM}{F.RED}({e.action}){S.RESET_ALL}")
-                print(tree(f"{F.BLUE}#{n}:{F.RESET}", e.state))
-        parser.exit(3, "\n".join(lines) + "\n")
-    if not args.verbose:
-        print()
-    if args.save is not None and explorer is not None:
-        explorer.save(args.save)
+        stderr.print(f"[red bold]error:[/] invalid assertion '{err.prop}'")
+        errname = err.err.__class__.__name__
+        stderr.print(f" :boom: [red]{errname}[/]: {err.err}",
+                     highlight=False)
+        raise Exit(3)
+    with stdout.status(explorer.status()) as status:
+        try:
+            import time
+            for state, succs in explorer:
+                status.update(explorer.status())
+                time.sleep(1)
+                if verbose:
+                    stdout.print(state_text(state),
+                                 f"=> [green]+{len(succs)}[/]",
+                                 highlight=False)
+        except KeyboardInterrupt:
+            pass
+        except AssertFailed as err:
+            stdout.print(f"[red bold]assert failed[/] '{err.prop}'")
+            if trace:
+                for n, e in enumerate(explorer.trace(err.state)):
+                    if e.trans is not None:
+                        stdout.print(f"[dim]>>[/] [cyan]system.{e.trans}[/]",
+                                     f"[dim]=>[/] [green]{e.action}[/green]",
+                                     highlight=False)
+                    stdout.print(f"[yellow]#{n}", state_text(e.state),
+                                 highlight=False)
+    if save is not None:
+        explorer.save(save)
+
+
+if __name__ == "__main__":
+    app()
